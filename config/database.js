83b7dfc5bb0ead94fa25cddbd61ec28c68330156
Mathilde <mathilde.ageron@ecoles-epsi.net>
2026-02-24 18:24:27 +0100

    update variable d'env

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize( 
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PWD, 
+    process.env.POSTGRES_DB, 
+    process.env.POSTGRES_USER, 
+    process.env.POSTGRES_PASSWORD, 
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
         dialect: 'postgres',
         logging: false, // true pour avoir les logs
     }
